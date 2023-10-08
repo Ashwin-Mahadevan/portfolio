b2ade3c6f26637f405fd6cf3ab5392b67cb68f70
Ashwin Mahadevan <mahadevan@ashwinm.com>
2023-10-07 19:19:04 -0700

    Edge Runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import React, { type ReactNode, type FunctionComponent } from "react";
 import "@/styles/globals.css";
 
+export const runtime = "edge";
+
 type Props = { children: ReactNode };
 
 const Layout: FunctionComponent<Props> = ({ children }) => (
