958f9c706e6402e700cfcf7872a7aa51074db2d1
Ashwin Mahadevan <mahadevan@ashwinm.com>
2024-07-17 14:09:04 -0700

    Styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,50 @@
-import type { ReactNode } from "react";
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { ThemeProvider } from "@/components/theme";
-import { Noto_Serif_Display, Noto_Sans_Display } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import { Noto_Sans_Display as NotoSansDisplay, Noto_Serif_Display as NotoSerifDisplay } from "next/font/google";
+import Link from "next/link";
+import type { ReactNode } from "react";
 
 export const runtime = "edge";
 
-const serif = Noto_Serif_Display({
-	subsets: ["latin"],
-	variable: "--font-serif",
-});
+const sans = NotoSansDisplay({ subsets: ["latin"], variable: "--font-sans" });
+const serif = NotoSerifDisplay({ subsets: ["latin"], variable: "--font-serif" });
 
-const sans = Noto_Sans_Display({
-	subsets: ["latin"],
-	variable: "--font-sans",
-});
-
-const menulinks = [
-	{ href: "/about", text: "About Me" },
-	{ href: "/blog", text: "Blog" },
-];
-
-type RootLayoutProps = { children: ReactNode };
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout(props: { children: ReactNode }) {
 	return (
 		<html
 			lang="en"
-			// We add the `dark` class here so that Tailwind doesn't purge the dark mode styles.
-			// We suppress the hydration warning since this class will be removed by next-themes before hydration.
+			// The `dark` class is only here to ensure that TailwindCSS doesn't purge the dark mode styles.
+			// It may be removed by `next-themes` while rendering.
 			className="dark"
+			// This is needed due to `next-themes`.
 			suppressHydrationWarning
 		>
-			<body className={`${serif.variable} ${sans.variable} font-sans`}>
-				<ThemeProvider>
-					<nav className="container sticky top-0 mx-auto flex h-16 items-center">
-						<div className="flex flex-1">
-							<h1 className="font-serif text-3xl font-bold">Ashwin Mahadevan</h1>
+			<body className={`${sans.variable} ${serif.variable} font-sans`}>
+				<ThemeProvider attribute="class">
+					<nav className="container sticky top-0 mx-auto my-4 flex-col items-center justify-center">
+						<div className="flex items-center justify-center">
+							<Link href="/">
+								<h1 className="font-serif text-3xl font-bold">Ashwin Mahadevan</h1>
+							</Link>
 						</div>
-						<div className="flex-0 flex gap-4">
-							{menulinks.map((link, index) => (
-								<a key={index} href={link.href} className="text-lg font-semibold">
-									{link.text}
-								</a>
-							))}
+
+						<div className="flex items-center justify-center gap-8">
+							<Link href="/about">
+								<p className="text-lg">About</p>
+							</Link>
+
+							<Link href="/about">
+								<p className="text-lg">Work</p>
+							</Link>
+
+							<Link href="/contact">
+								<p className="text-lg">Contact</p>
+							</Link>
 						</div>
 					</nav>
 
-					<main>{children}</main>
+					<main>{props.children}</main>
 				</ThemeProvider>
 
 				<Analytics />
