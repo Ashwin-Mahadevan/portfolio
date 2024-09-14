ed23d216e707a6ffb82fb9e9ea7c1dfc522175eb
Ashwin Mahadevan <mahadevan@ashwinm.com>
2024-09-13 20:31:59 -0700

    Unused Code

--- src/components/theme.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ThemeProvider as NextThemeProvider } from "next-themes";
-import type { ReactNode } from "react";
-
-export function ThemeProvider(props: { children: ReactNode }) {
-	return <NextThemeProvider attribute="class">{props.children}</NextThemeProvider>;
-}
