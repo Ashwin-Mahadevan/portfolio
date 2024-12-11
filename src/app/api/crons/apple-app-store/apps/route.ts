4d74b52ee24dff1ea9b308ad53055af7fc8a1e22
Ashwin Mahadevan <mahadevan@ashwinm.com>
2024-12-11 08:31:13 -0800

    Vercel Cron Schedule

--- src/app/api/crons/apple-app-store/apps/route.ts
+++ src/app/api/crons/apple-app-store/apps/route.ts
@@ -1,6 +1,18 @@
 import { db, schema } from "@/lib/db";
 import { z } from "zod";
-import countries from "./countries.json";
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz" as const;
+const types = ["free", "paid"] as const;
+
+function* list() {
+	for (const first_letter of alphabet) {
+		for (const second_letter of alphabet) {
+			for (const type of types) yield { country: `${first_letter}${second_letter}`, type };
+		}
+	}
+}
+
+const countries = Array.from(list());
 
 const rss_schema = z.object({
 	feed: z.object({
@@ -19,39 +31,34 @@ const rss_schema = z.object({
 	}),
 });
 
-async function handle(request: Request) {
-	await Promise.all(countries.map(({ country, type }) => scrape(country, type)));
+async function handle() {
+	const results = countries.map(({ country, type }) =>
+		fetch(`https://rss.applemarketingtools.com/api/v2/${country}/apps/top-${type}/100/apps.json`)
+			.then(async (response) => rss_schema.parse(await response.json()))
+			.then(({ feed }) =>
+				db
+					.insert(schema.apple_app_store_top_apps)
+					.values({
+						country: feed.country,
+						updated: feed.updated,
+						type,
+						results: feed.results.map((result) => ({
+							id: result.id,
+							name: result.name,
+							artist: result.artistName,
+							release: result.releaseDate,
+							url: result.url,
+						})),
+					})
+					.execute(),
+			)
+			.then(() => console.log(`Scraped: country='${country}', type='${type}'`))
+			.catch(() => console.error(`Failed to Scrape: country='${country}', type='${type}'`)),
+	);
+
+	await Promise.allSettled(results);
 	return new Response("OK");
 }
 
 export const GET = handle;
 export const POST = handle;
-
-const scrape = async (country: string, type: string) => {
-	if (type !== "free" && type !== "paid") throw new Error("invalid type");
-
-	const response = await fetch(`https://rss.applemarketingtools.com/api/v2/${country}/apps/top-${type}/100/apps.json`);
-
-	// TODO: Handle Error.
-	if (!response.ok) {
-		console.log(response.status, response.statusText, await response.text());
-		throw new Error("bad response");
-	}
-
-	console.log("OK");
-
-	const { feed } = rss_schema.parse(await response.json());
-
-	await db.insert(schema.apple_app_store_top_apps).values({
-		country: feed.country,
-		updated: feed.updated,
-		type,
-		results: feed.results.map((result) => ({
-			id: result.id,
-			name: result.name,
-			artist: result.artistName,
-			release: result.releaseDate,
-			url: result.url,
-		})),
-	});
-};
