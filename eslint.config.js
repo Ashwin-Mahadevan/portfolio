9d5cf1d43a2e68e99d5ffbae305b5e97478b1270
Ashwin Mahadevan <mahadevan@ashwinm.com>
2025-01-05 13:12:28 -0800

    Formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
+import typescript_plugin from "@typescript-eslint/eslint-plugin";
+import typescript_parser from "@typescript-eslint/parser";
+import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 const compat = new FlatCompat({
 	baseDirectory: __dirname,
 	recommendedConfig: js.configs.recommended,
@@ -20,18 +20,14 @@ export default [
 		"plugin:@typescript-eslint/stylistic-type-checked",
 	),
 	{
-		plugins: {
-			"@typescript-eslint": typescriptEslint,
-		},
+		plugins: { typescript_plugin },
 
 		languageOptions: {
-			parser: tsParser,
 			ecmaVersion: 5,
 			sourceType: "script",
 
-			parserOptions: {
-				project: true,
-			},
+			parser: typescript_parser,
+			parserOptions: { project: true },
 		},
 
 		rules: {
