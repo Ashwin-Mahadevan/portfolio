adc2d2f7eb2c5799661b2517e1f0eaa9d747515c
Ashwin Mahadevan <mahadevan@ashwinm.com>
2025-08-31 11:40:05 -0700

    useMDXComponents

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,31 +1,35 @@
 import Link from "next/link";
-import type { ReactNode, ComponentProps, Component } from "react";
+import type { ReactNode, ComponentProps } from "react";
+import type { MDXComponents } from "mdx/types";
 
-export function useMDXComponents(components: Record<string, Component>) {
-	return {
-		...components,
 
-		h1(props: ComponentProps<"h1">) {
-			return (
-				<h1 className="text-balance tracking-tighter">{props.children}</h1>
-			);
-		},
+const components = {
+	h1(props: ComponentProps<"h1">) {
+		return (
+			<h1 className="text-balance tracking-tighter">{props.children}</h1>
+		);
+	},
 
-		h2(props: ComponentProps<"h2">) {
-			return <h2 className="text-balance tracking-tight">{props.children}</h2>;
-		},
+	h2(props: ComponentProps<"h2">) {
+		return <h2 className="text-balance tracking-tight">{props.children}</h2>;
+	},
 
-		a(props: ComponentProps<"a">) {
-			return (
-				<Link href={props.href!} className="whitespace-nowrap">
-					{props.children}
-				</Link>
-			);
-		},
+	a(props: ComponentProps<"a">) {
+		const href = props.href ?? "#";
 
-		// TODO: Manually add styles instead of using tailwind prose.
-		wrapper(props: { children: ReactNode }) {
-			return <div className="prose">{props.children}</div>;
-		},
-	};
+		return (
+			<Link href={href} className="whitespace-nowrap">
+				{props.children}
+			</Link>
+		);
+	},
+
+	// TODO: Manually add styles instead of using tailwind prose.
+	wrapper(props: { children: ReactNode }) {
+		return <div className="prose">{props.children}</div>;
+	},
+} as const satisfies MDXComponents;
+
+export function useMDXComponents(): MDXComponents {
+	return components;
 }
