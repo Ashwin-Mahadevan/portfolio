ba7e092a363e9b25a9c1abef078c799ce582d21b
Ashwin Mahadevan <mahadevan@ashwinm.com>
2025-11-16 11:33:41 -0500

    themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import type { ReactNode } from "react";
+import { ThemeProvider } from "@/components/theme";
 
 export const metadata = {
   title: "Ashwin Mahadevan",
@@ -8,8 +9,14 @@ export const metadata = {
 
 export default function RootLayout(props: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>{props.children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-mauve-base-subtle">
+        <ThemeProvider>
+          <main className="container mx-auto bg-mauve-base min-h-screen px-4 sm:px-8 lg:px-16">
+            {props.children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
